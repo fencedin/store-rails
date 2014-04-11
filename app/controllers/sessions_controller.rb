a99359ed532b7ce5b139c94586a7aa9bdd9d118d
Vincent Rotter & Liz Tom <student@epicodus.com>
2014-04-11 10:42:31 -0700

    add admins

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
+
   def new
   end
+
+  def create
+    user = User.find_by_name(params[:name])
+    if user && user.authenticate(params[:password])
+      redirect_to products_path, notice: "Logged in!"
+    else
+      flash[:alert] = "Name or password is invalid"
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to admin_path, notice: "Logged out!"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Amazan::Application.routes.draw do
+  get 'new-admin', to: 'users#new', as: 'newadmin'
+  get 'admin', to: 'sessions#new', as: 'admin'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
   resources :users
   resources :sessions
 end
